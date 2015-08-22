bf818e6ea774d15108a26d802ea125f470bd0dd2
cheatnotz <s7070007@kmitl.ac.th>
2015-08-22 13:09:36 +0700

    FT.L0201.ADDFILE

---
+++ SEMESTER1_2558/OOP/LAB02/LAB02_01.java
@@ -0,0 +1,15 @@
+package lab02;
+
+public class LAB02_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count = 1;
+		do {
+			System.out.print(count + " ");
+			count += 2;
+		}
+		while(count <= 100);
+	}
+
+}
