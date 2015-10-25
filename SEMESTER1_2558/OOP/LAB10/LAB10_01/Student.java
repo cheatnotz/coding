f1269d6539d05efe3fbfeb3012bd29a4e3112a66
cheatnotz <s7070007@kmitl.ac.th>
2015-10-25 11:13:35 -0700

    FT.L1001.ADDDIR

---
+++ SEMESTER1_2558/OOP/LAB10/LAB10_01/Student.java
@@ -0,0 +1,42 @@
+/**
+ * Created by LAB203-28 on 10/25/2015.
+ */
+public class Student {
+    private int id;
+    private String firstname;
+    private String lastname;
+
+    public Student(int id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String toString() {
+        return String.format("%d: %s %s", getId(), getFirstname(), getLastname());
+    }
+}
